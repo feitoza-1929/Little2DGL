a8418f39130adc7b65874ee9459ca3a608bebc36
Feitoza <thiagobritofeitoza@gmail.com>
2024-01-06 15:31:25 -0300

    [FEAT]: implemented cavas system

---
+++ src/Canvas.cpp
@@ -0,0 +1,54 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+
+#include <Canvas.hpp>
+
+namespace L2DGL::Canvas
+{
+        Canvas::Canvas(int width, int height, char* title)
+        {
+            _width = width;
+            _height = height;
+
+            glfwInit();
+
+            _canvasReference = glfwCreateWindow(width, height, title, NULL, NULL);
+            glfwMakeContextCurrent(_canvasReference);
+
+            glewInit();
+            return;
+        }
+
+        bool Canvas::isOpen()
+        {
+            return !glfwWindowShouldClose(_canvasReference);
+        }
+
+        void Canvas::close()
+        {
+            glfwDestroyWindow(_canvasReference);
+            glfwTerminate();
+        }
+
+        void Canvas::clean()
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+
+        void Canvas::flip()
+        {
+            glfwSwapBuffers(_canvasReference);
+            glfwPollEvents();
+        }
+
+        int Canvas::getWidth()
+        {
+            return _width;
+        }
+
+        int Canvas::getHeight()
+        {
+            return _height;
+        }
+}
