acd8cc253820858e034efe578d2ae9f7768d7ca9
Feitoza <thiagobritofeitoza@gmail.com>
2024-01-14 19:19:54 -0300

    [MATH:FEAT]: added a simple 2D vector

---
+++ include/Vector2.hpp
@@ -0,0 +1,15 @@
+#ifndef __VECTOR_2_HPP
+#define __VECTOR_2_HPP
+
+namespace L2DGL::Math
+{
+    class Vector2
+    {
+    public:
+        Vector2();
+        Vector2(float x, float y);
+        float x;
+        float y;
+    };
+}
+#endif
