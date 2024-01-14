60270170462fd31cfd9c172410f014b83eedb7dc
Feitoza <thiagobritofeitoza@gmail.com>
2024-01-14 19:22:59 -0300

    [GRAPHICS:FEAT] added scale, position properties

--- include/QuadShape.hpp
+++ include/QuadShape.hpp
@@ -1,22 +1,28 @@
+#include <Vector2.hpp>
+
 #ifndef __QUAD_SHAPE_HPP
 #define __QUAD_SHAPE_HPP
+
+using namespace L2DGL::Math;
 
 namespace L2DGL::Graphics
 {
     class QuadShape
     {
     private:
-        int _width;
-        int _height;
+        Vector2 _position;
+        Vector2 _scale;
         int _vectorSize = 2;
         int _verticesCount = 6;
-        unsigned int _VBO;
-        unsigned int _VAO;
+        unsigned int _VBO, _VAO, _EBO;
 
     public:
-        QuadShape(float width, float height);
+        QuadShape(Vector2 scale);
         void draw();
-        
+        Vector2 getScale();
+        void setScale(Vector2 scale);
+        Vector2 getPosition();
+        void setPosition(Vector2 position);
     };
 }
 #endif
