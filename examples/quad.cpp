e2cacdaee0de47c363ec0cf93a4d4654041ca433
Feitoza <thiagobritofeitoza@gmail.com>
2024-01-20 03:54:23 -0300

    [EXAMPLES:REFACTOR] added vector as parameter to build quad

--- examples/quad.cpp
+++ examples/quad.cpp
@@ -1,13 +1,15 @@
 #include <QuadShape.hpp>
 #include <Canvas.hpp>
+#include <Vector2.hpp>
 
 using namespace L2DGL::Graphics;
 using namespace L2DGL::Canvas;
+using namespace L2DGL::Math;
 
 int main()
 {
     Canvas canvas(800, 600, "Test");
-    QuadShape quad(0.5, 0.4);
+    QuadShape quad(Vector2(0.4, 0.3));
 
     while (canvas.isOpen())
     {
