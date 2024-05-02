30ebbc88187d3cc58a085665d9f4dc5d9c14ad9c
Feitoza <thiago.feitoza@meucorban.app>
2024-05-02 09:56:13 -0300

    [SHADER:REFACTOR] setting position transformation

--- src/Renderable.cpp
+++ src/Renderable.cpp
@@ -6,8 +6,13 @@ namespace L2DGE::Graphics
       : _position(&position), _size(&size), _color(&color), _primitiveType(primitiveType), _vertexCount(8)
   {
     _shader = new Shader("/home/feitoza/Sources/personal/Little2DGraphicsLib/src/DefaultVertexShader.vert", "/home/feitoza/Sources/personal/Little2DGraphicsLib/src/DefaultFragmentShader.frag");
+    
     _shader->activate();
     _shader->setVariableFloat4("u_Color", new float[4]{color.r, color.g, color.b, color.a});
+    
+    glm::mat4 transformation = glm::mat4(1.0);
+    transformation = glm::translate(transformation, glm::vec3(position.x, position.y, 0));
+    _shader->setVariableMatrix4("u_Transform",glm::value_ptr(transformation));
 
     float positions[]
     {
@@ -27,14 +32,7 @@ namespace L2DGE::Graphics
     glBindVertexArray(_VAO);
 
     _VBO = new VertexBuffer(GL_ARRAY_BUFFER,  positions, sizeof(positions));
-    //glGenBuffers(1, &_VBO);
-    //glBindBuffer(GL_ARRAY_BUFFER, _VBO);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-
     _EBO = new VertexBuffer(GL_ELEMENT_ARRAY_BUFFER,  indices, sizeof(indices));
-    //glGenBuffers(1, &_EBO);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
@@ -62,23 +60,34 @@ namespace L2DGE::Graphics
     _shader->activate();
     _shader->setVariableFloat4("u_Color", new float[4]{color.r, color.g, color.b, color.a});
   }
+  
   void Renderable::move(const Vector2 &position)
   {
-    Transform transform;
-    transform.translate(position);
-    transform.print();
+    _position->x += position.x;
+    _position->y += position.y;
+    
+    glm::mat4 transformation = glm::mat4(1.0);
+    transformation = glm::translate(transformation, glm::vec3(_position->x, _position->y, 0));
     
     _shader->activate();
-    _shader->setVariableMatrix4("u_Transform", transform.transformation);
+    _shader->setVariableMatrix4("u_Transform",glm::value_ptr(transformation));
   }
-  void Renderable::rotate(float angle){}
+  
+  void Renderable::rotate(float angle)
+  {
+    glm::mat4 transformation = glm::mat4(1.0);
+    transformation = glm::rotate(transformation, glm::radians(angle), glm::vec3(0.0, 0.0, 1.0));
+   
+    _shader->activate();
+    _shader->setVariableMatrix4("u_Transform",glm::value_ptr(transformation));
+  }
+  
   void Renderable::scale(const Vector2 &scaleOffset)
   {
-    Transform transform;
-    transform.scale(scaleOffset);
-    transform.print();
+    glm::mat4 transformation = glm::mat4(1.0);
+    transformation = glm::scale(transformation, glm::vec3(scaleOffset.x, scaleOffset.y, 0));
     
     _shader->activate();
-    _shader->setVariableMatrix4("u_Transform", transform.transformation);
+    _shader->setVariableMatrix4("u_Transform", glm::value_ptr(transformation));
   }
 }
